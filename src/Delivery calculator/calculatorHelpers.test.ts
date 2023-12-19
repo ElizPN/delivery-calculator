552d7ebb60078f872ad37ea6f92f611b054a0c73
ElizPN <pankevych.liza@gmail.com>
2023-12-19 12:34:03 +0100

    add test

---
+++ src/Delivery calculator/calculatorHelpers.test.ts
@@ -0,0 +1,17 @@
+
+import { getSurchargeForLowCartValue } from './calculatorHelpers';
+
+describe("getSurchargeForLowCartValue", () => {
+
+    test("should return 0 when cart value more than 10", () => {
+        const result = getSurchargeForLowCartValue(12)
+        expect(result).toEqual(0)
+    });
+
+    test("should return difference whwn cart value less than 10", () => {
+        const result = getSurchargeForLowCartValue(8)
+        expect(result).toEqual(2)
+    });
+
+
+});
