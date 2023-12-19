576a375bf22e7c3410f4580ed25944b101d70657
ElizPN <pankevych.liza@gmail.com>
2023-12-19 14:55:45 +0100

    feat: get delivery from distance

--- src/Delivery calculator/calculatorHelpers.test.ts
+++ src/Delivery calculator/calculatorHelpers.test.ts
@@ -1,5 +1,5 @@
 
-import { getSurchargeForCartValue } from './calculatorHelpers';
+import { getSurchargeForCartValue, getSurchargeForDistance } from './calculatorHelpers';
 
 describe("getSurchargeForLowCartValue", () => {
 
@@ -13,5 +13,43 @@ describe("getSurchargeForLowCartValue", () => {
         expect(result).toEqual(2)
     });
 
+
+});
+
+
+describe("getSurchargeForDistance", () => {
+
+    test("should return 1 when distance between 0 and 500", () => {
+        const result = getSurchargeForDistance(500)
+        expect(result).toEqual(1)
+    });
+
+    test("should return 2 when distance between 501 and 1000", () => {
+        const result = getSurchargeForDistance(501)
+        expect(result).toEqual(2)
+    });
+
+    test("should return 2 also when distance between 501 and 1000", () => {
+        const result = getSurchargeForDistance(999)
+        expect(result).toEqual(2)
+    });
+
+    test("should return 3 when distance between 1001 and 1500", () => {
+
+        expect(getSurchargeForDistance(1001)).toEqual(3)
+        expect(getSurchargeForDistance(1500)).toEqual(3)
+    });
+
+    test("should return 4 when distance between 1500 and 2000", () => {
+
+        expect(getSurchargeForDistance(1501)).toEqual(4)
+        expect(getSurchargeForDistance(2000)).toEqual(4)
+    });
+
+
+
+    test("should return 5 when distance between 2001 and 2500", () => {
+        expect(getSurchargeForDistance(2001)).toEqual(5)
+    });
 
 });
--- src/Delivery calculator/calculatorHelpers.ts
+++ src/Delivery calculator/calculatorHelpers.ts
@@ -5,3 +5,16 @@ export function getSurchargeForCartValue(cartValue: number) {
     return 10 - cartValue
 
 }
+
+
+export function getSurchargeForDistance(distance: number) {
+    if (distance <= 500) {
+        return 1
+    }
+    let result = distance / 500
+    result = Math.ceil(result)
+
+    return result
+}
+
+getSurchargeForDistance(1001)
