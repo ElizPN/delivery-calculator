88d36ac3537199fdb983b3f5a45f240f8586c125
ElizPN <pankevych.liza@gmail.com>
2023-12-20 15:02:10 +0100

    add unit tests and helpers functions

---
+++ src/DeliveryCalculator/calculatorHelpers.ts
@@ -0,0 +1,35 @@
+export function getSurchargeForCartValue(cartValue: number) {
+    if (cartValue > 10) {
+        return 0
+    }
+
+    return 10 - cartValue
+
+}
+
+
+export function getSurchargeForDistance(distance: number) {
+    if (distance <= 500) {
+        return 1
+    }
+
+    let result = distance / 500
+
+    return Math.ceil(result)
+}
+
+
+export function getSurchargeForCartsCount(cartsCount: number) {
+    if (cartsCount < 5) {
+        return 0
+    }
+
+    if (cartsCount >= 5 && cartsCount < 13) {
+        return (cartsCount - 4) * 0.50
+    }
+
+    const firstFee = 8 * 0.50
+    const secondFeee = (cartsCount - 12) * 1.20
+ 
+    return firstFee + secondFeee
+}
